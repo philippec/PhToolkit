7ab1f2b8cd5db5621c38c4bdb5f2dc215ce49963
Philippe Casgrain <philippe@casgrain.com>
2010-10-11 12:11:25 -0400

    Unit test target

---
+++ tests/Tests.h
@@ -0,0 +1,17 @@
+//
+//  Tests.h
+//  PhToolkit
+//
+//  Created by Philippe on 10-10-11.
+//  Copyright 2010 Philippe Casgrain. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Tests : SenTestCase
+{
+
+}
+
+@end
---
+++ tests/Tests.m
@@ -0,0 +1,14 @@
+//
+//  Tests.m
+//  PhToolkit
+//
+//  Created by Philippe on 10-10-11.
+//  Copyright 2010 Philippe Casgrain. All rights reserved.
+//
+
+#import "Tests.h"
+
+
+@implementation Tests
+
+@end
