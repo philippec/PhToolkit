448c57c5d2f74ddb70f7eb8e7a30f1dfc3ab95b6
Philippe Casgrain <philippe@casgrain.com>
2010-10-11 12:26:38 -0400

    Initial date tests with ancient and future.

--- tests/Tests.m
+++ tests/Tests.m
@@ -11,4 +11,16 @@
 
 @implementation Tests
 
+- (void) testDateComparison
+{
+    NSDate *ancient = [NSDate distantPast];
+    NSDate *future = [NSDate distantFuture];
+
+    STAssertFalse([ancient isEqualToDate: future], @"ancient date should not equal future date");
+    STAssertTrue([ancient earlierDate: future] == ancient, @"ancient date should be earlier than future date");
+    STAssertTrue([future laterDate: ancient] == future, @"ancient date should be earlier than future date");
+    STAssertTrue([ancient compare: future] == NSOrderedAscending, @"ancient date should be earlier than future date");
+    STAssertTrue([future compare: ancient] == NSOrderedDescending, @"ancient date should be earlier than future date");
+}
+
 @end
